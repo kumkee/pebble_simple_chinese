bfb1119d2a92ee4d3d64b5fbc3283f0466a1b214
kumkee <kumkeejjl@gmail.com>
2013-10-28 11:24:52 +0800

    minor change to weather.php

--- server/yahoo-weather.php
+++ server/yahoo-weather.php
@@ -67,21 +67,39 @@ print json_encode($data);
 
 $geojson = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?latlng=' . $lat . ',' . $long . '&sensor=false&language=zh-CN');
 $geodata = json_decode($geojson, TRUE);
-$addcomp = $geodata["results"][0]["address_components"];
+//$addcomp = $geodata["results"][0]["address_components"];
+$jsonresult = $geodata["results"];
+$i = 0;
+$pc_idx = 0;
+$sa_idx = -1;
+foreach($jsonresult as $result){
+   if($result["types"][0] == "street_address")
+   {
+	$sa_idx = $i;
+	break;
+   }
+   else if($result["types"][0] == "postal_code")
+	$pc_idx =$i;
+   $i++;
+}
+$addcomp = $jsonresult[$sa_idx<0?$pc_idx:$sa_idx]["address_components"];
 
-$city_idx = 0; $i = 0;
+$country_idx = 0; 
+$i = 0;
 foreach ($addcomp as $componet){
-   if($componet["types"][0] == "locality")
-	$city_idx = $i;
+   if($componet["types"][0] == "country")
+   {
+	$country_idx = $i;
+	break;
+   }
    $i++;
 }
 
-$city = $addcomp[$city_idx]["short_name"];
-$prov = $addcomp[$city_idx+1]["short_name"];
-$country = $addcomp[$city_idx+2]["short_name"];
-$district = $addcomp[$city_idx-1]["short_name"];
+$district = $addcomp[$country_idx-3]["short_name"];
+$city = $addcomp[$country_idx-2]["short_name"];
+$prov = $addcomp[$country_idx-1]["short_name"];
+$country = $addcomp[$country_idx]["short_name"];
 
-//$tbl_name = $pebbleid;
 //$tbl_name = "test";
 $tbl_name = str_replace(":", "", $pebbleid);
 $create_table =
